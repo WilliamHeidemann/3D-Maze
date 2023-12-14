6e79766eda1953c1b0473a89bc64c56d2cf1d4bb
William Skou Heidemann <Williamskou2227@gmail.com>
2023-12-14 13:06:15 +0100

    set up steamworks for leaderboard

---
+++ Assets/LeaderBoard.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Steamworks;
+using TMPro;
+
+public class LeaderBoard : MonoBehaviour
+{
+    [SerializeField] private List<TextMeshProUGUI> entriesNames;
+    [SerializeField] private List<TextMeshProUGUI> entriesScore;
+
+    private const string GlobalLeaderboardName = "GlobalHighscores";
+    private SteamLeaderboard_t _leaderboard;
+    private CallResult<LeaderboardScoresDownloaded_t> _onLeaderboardScoresDownloaded;
+    
+    private void Start()
+    {
+        if (!SteamManager.Initialized) { print("Steam Manager is not initialized"); return; }
+        var handle = SteamUserStats.FindLeaderboard(GlobalLeaderboardName);
+        _leaderboard = new SteamLeaderboard_t(handle.m_SteamAPICall);
+        _onLeaderboardScoresDownloaded = CallResult<LeaderboardScoresDownloaded_t>.Create(OnLeaderboardScoresDownloaded);
+        _onLeaderboardScoresDownloaded.Set(handle);
+    }
+
+    private void OnLeaderboardScoresDownloaded(LeaderboardScoresDownloaded_t pCallback, bool bIOFailure)
+    {
+        if (bIOFailure) { Debug.LogError("Leaderboard fetch failed!"); return; }
+
+        _leaderboard = pCallback.m_hSteamLeaderboard;
+        
+        var numEntries = Mathf.Min(pCallback.m_cEntryCount, 10);
+        var entries = new List<LeaderboardEntry_t>();
+        for (var i = 0; i < numEntries; i++)
+        {
+            SteamUserStats.GetDownloadedLeaderboardEntry(pCallback.m_hSteamLeaderboardEntries, i, out var entry, null, 0);
+            entries.Add(entry);
+        }
+        DisplayEntries(entries);
+        print("No more entries.");
+
+        UploadScore(100);
+    }
+
+    private void DisplayEntries(List<LeaderboardEntry_t> entries)
+    {
+        print(entries.Count);
+        for (int i = 0; i < 10; i++)
+        {
+            // print($"ID {entries[i].m_steamIDUser}: {entries[i].m_nScore}");
+            var playerName = entries.Count > i ? entries[i].m_steamIDUser.ToString() : "Test Player";
+            var playerScore = entries.Count > i ? entries[i].m_nScore.ToString() : "99999";
+            entriesNames[i].text = playerName;
+            entriesScore[i].text = playerScore;
+        }
+    }
+
+    private void UploadScore(int score)
+    {
+        SteamUserStats.UploadLeaderboardScore(_leaderboard,
+            ELeaderboardUploadScoreMethod.k_ELeaderboardUploadScoreMethodKeepBest, score, null, 0);
+        print($"Score of {score} uploaded.");
+    }
+
+    public void FetchLeaderboard()
+    {
+        if (!SteamManager.Initialized) return;
+        var leaderboardCallback = SteamUserStats.FindLeaderboard(GlobalLeaderboardName);
+        var leaderboard = new SteamLeaderboard_t(leaderboardCallback.m_SteamAPICall);
+        
+        var entriesCallback = SteamUserStats.DownloadLeaderboardEntries(leaderboard,
+            ELeaderboardDataRequest.k_ELeaderboardDataRequestGlobal, 1, 10);
+    
+        var entries = new SteamLeaderboardEntries_t(entriesCallback.m_SteamAPICall);
+    }
+}
